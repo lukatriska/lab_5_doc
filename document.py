a375380a31c81ba2d7ab348889c6ddb0344d3a8e
lukatriska <triska@ucu.edu.ua>
2017-03-07 09:38:03 +0200

    final

--- document.py
+++ document.py
@@ -1,48 +1,144 @@
+class CursorBeforeStart(Exception):
+    pass
+
+
+class CursorAfterEnd(Exception):
+    pass
+
+
+class NonexistentChar(Exception):
+    pass
+
+
+class FileHasNoName(Exception):
+    pass
+
+
+class MultipleCharsEntered(Exception):
+    pass
+
+
 class Document:
+    """
+    Represents a document with characters and cursor position (represented by the Cursor class).
+    """
     def __init__(self):
             self.characters = []
-            self.cursor = 0
-            self.filename = ''
+            self.cursor = Cursor(self)
 
     def insert(self, character):
-        self.characters.insert(self.cursor, character)
-        self.cursor += 1
+        """
+        Inserts a given character. Raises MultipleCharsEntered if more than one character has been entered.
+        :param character: str
+        """
+        if len(character) > 1:
+            raise MultipleCharsEntered
+        else:
+            self.characters.insert(self.cursor.position, character)
+            self.cursor.forward()
 
     def delete(self):
-        del self.characters[self.cursor]
+        """
+        Deletes a character, raises NonexistentChar if a character does not exist.
+        """
+        if self == "":
+            raise NonexistentChar
+        else:
+            del self.characters[self.cursor.position]
 
-    def save(self):
-        f = open(self.filename, 'w')
-        f.write(''.join(self.characters))
-        f.close()
+    def save(self, filename=''):
+        """
+        Saves the information to a file.
+        :param filename: str (name of the file)
+        """
+        if filename == '':
+            raise FileHasNoName
+        else:
+            f = open(filename, 'w')
+            f.write(''.join(self.characters))
+            f.close()
 
     def forward(self):
-        self.cursor += 1
+        """
+        Moves the cursor forwards.
+        """
+        self.cursor.position += 1
 
     def back(self):
-        self.cursor -= 1
+        """
+        Moves the cursor backwards.
+        """
+        self.cursor.position -= 1
+
+    @property
+    def string(self):
+        """
+        Returns the characters in a normal form.
+        :return: str
+        """
+        return "".join(self.characters)
 
 
 class Cursor:
-
+    """
+    Represents a cursor (position in in a file) with a document (Document object) and a position.
+    """
     def __init__(self, document):
-
         self.document = document
         self.position = 0
 
     def forward(self):
-        self.position += 1
+        """
+        Moves the cursor forwards.
+        """
+        if self.position > len(self.document.characters):
+            raise CursorAfterEnd
+        else:
+            self.position += 1
 
     def back(self):
-        self.position -= 1
-
-    def home(self):
-        while self.document.characters[self.position - 1] != '\n':
+        """
+        Moves the cursor backwards.
+        """
+        if self.position < self.document.characters:
+            raise CursorBeforeStart
+        else:
             self.position -= 1
 
+    def home(self):
+        """
+        Moves the cursor to the home position (the beginning of the line).
+        """
+        while self.document.characters[self.position - 1] != '\n':
+            self.position -= 1
             if self.position == 0:  # Got to beginning of file before newline
                 break
 
     def end(self):
+        """
+        Moves the cursor to the end position (the end of the line).
+        """
         while self.position < len(self.document.characters) and self.document.characters[self.position] != '\n':
             self.position += 1
+
+
+class Character:
+    def __init__(self, character, bold=False, italic=False, underline=False):
+        """
+        Represents a character.
+        :param character: str
+        :param bold: bool
+        :param italic: bool
+        :param underline: bool
+        """
+        assert len(character) == 1
+        self.character = character
+        self.bold = bold
+        self.italic = italic
+        self.underline = underline
+
+    def __str__(self):
+        bold = "*" if self.bold else ''
+        italic = "/" if self.italic else ''
+        underline = "_" if self.underline else ''
+        return bold + italic + underline + self.character
--- test_document.py
+++ test_document.py
@@ -1,2 +1,46 @@
-чи були твори популярні за життя/смерті
-шо найбілььше запам'яталося
+import document
+
+document = document.Document()
+
+# shows insertion
+document.insert("p")
+document.insert("r")
+document.insert("o")
+document.insert("d")
+document.insert("i")
+document.insert("g")
+document.insert("y")
+print(document.string)
+
+# shows moving back
+document.back()
+document.back()
+document.back()
+document.back()
+document.insert("X")
+print(document.string)
+
+# shows moving forwards
+document.forward()
+document.forward()
+document.insert("X")
+print(document.string)
+
+# shows moving home
+document.cursor.home()
+document.insert("X")
+print(document.string)
+
+# shows moving to the end
+document.cursor.end()
+document.insert("X")
+print(document.string)
+
+# shows deleting a character
+document.back()
+document.back()
+document.delete()
+print(document.string)
+
+# shows saving a file
+document.save("test")
